357befab093d361e00373e8eee80076aa6dab0de
Japhet Tegtmeyer <japhteg@gmail.com>
2023-10-23 16:34:46 -0400

    Initial Commit

---
+++ CountUP!/CountUP_App.swift
@@ -0,0 +1,17 @@
+//
+//  CountUP_App.swift
+//  CountUP!
+//
+//  Created by Japhet Tegtmeyer on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct CountUP_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
