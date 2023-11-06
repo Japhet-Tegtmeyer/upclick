1ef1ec6fb472390ad407337722d97561d480a50d
Japhet Tegtmeyer <japhteg@gmail.com>
2023-11-05 20:13:13 -0500

    first commit

---
+++ CountUP!/Components/MainButton.swift
@@ -0,0 +1,18 @@
+//
+//  MainButton.swift
+//  CountUP!
+//
+//  Created by Japhet Tegtmeyer on 11/5/23.
+//
+
+import SwiftUI
+
+struct MainButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainButton()
+}
